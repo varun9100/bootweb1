5fead4d7875fa5fe639bfe667a2d5b419558870c
varun <varun9100@gmail.com>
2020-05-30 13:09:28 -0500

    first commit

---
+++ src/main/java/com/varun/springweb/Bootweb1Application.java
@@ -0,0 +1,13 @@
+package com.varun.springweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootweb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootweb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/varun/springweb/Bootweb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varun.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootweb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
